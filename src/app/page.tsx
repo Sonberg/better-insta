4a93de7348f00c35752dc4a62aa781708eda6ced
Per <per.sonberg@gmail.com>
2025-03-14 14:47:20 +0100

    update Home component to include a title and adjust styling for improved layout and visual hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import UploadGallery from '@/components/UploadGallery';
+import UploadGallery from "@/components/UploadGallery";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    <main className="min-h-screen">
+      <h1 className="text-4xl font-extrabold mx-4 mt-8 py-4">BetterInsta</h1>
       <UploadGallery />
     </main>
   );
