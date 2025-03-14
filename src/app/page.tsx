5ab74b46cf37d979dac8b5b78549d733900978d7
Per <per.sonberg@gmail.com>
2025-03-14 15:02:01 +0100

    remove title from Home component for a cleaner layout and adjust ImageGallery styling by removing unnecessary padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import { Camera } from "lucide-react";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <h1 className="flex items-center gap-3 text-4xl font-extrabold mx-4 mt-8 py-4 bg-gradient-to-r from-orange-600 to-orange-500 bg-clip-text text-transparent">
-        <Camera className="w-12 h-12 text-orange-600" />
-        BetterInsta
-      </h1>
       <UploadGallery />
     </main>
   );
--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -204,7 +204,7 @@ export default function ImageGallery() {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {data.pages.map((page, pageIndex) =>
           page.images.map((image) => (
             <div 
